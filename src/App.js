d059618ad7249078dcabf2c8c156d6f24718a78d
dayron23 <dayronf30@gmail.com>
2022-12-20 01:08:02 -0500

    preEntrega2+figueroa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './components/NavBar';
 import ItemLisContainer from './components/ItemLisContainer';
+import FunctionCounter from './components/FunctionCounter';
+import ClassCounter from './components/ClassCounter';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <ItemLisContainer greeting={"BIENVENIDOS CLIENTES"}/>
+      <FunctionCounter element={ClassCounter}/>
+      <ClassCounter/>
     </div>
   );
 }
---
+++ src/components/FunctionCounter.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const FunctionCounter = (props) =>{
+    
+    const[count, setCount] = useState(0)
+
+    const decrement =()=> {
+        if(count > 0){
+        setCount(count - 1)
+        }
+    }
+
+    const increment = () =>{
+        setCount(count + 1)
+    }
+
+    return(
+        <>
+            <h3>Function Counter</h3>
+            <h3>{count}</h3>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+        </>
+    )
+}
+
+export default FunctionCounter;
